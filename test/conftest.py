b9430ea912199fca7d25cd53e6f7f2207e9197a5
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-08-30 03:37:09 +0600

    tests: tear down compose project from pytest hooks
    Apparently the only way to do it reliably

--- test/conftest.py
+++ test/conftest.py
@@ -7,6 +7,28 @@ import pytest
 from . import container_util
 
 
+current_compose_project = None
+
+
+def teardown_compose_project():
+    global current_compose_project
+
+    if current_compose_project is not None:
+        project = current_compose_project
+        current_compose_project = None
+        project.down(_bg=True, _tee=False).wait()
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_keyboard_interrupt(excinfo):
+    teardown_compose_project()
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_internalerror(excrepr, excinfo):
+    teardown_compose_project()
+
+
 @pytest.fixture(scope='session')
 def global_tmp_path(tmp_path_factory):
     return tmp_path_factory.getbasetemp().parent
@@ -19,12 +41,16 @@ def xvfb_fbdir(tmp_path_factory):
 
 @pytest.fixture(scope='session')
 def compose_project(xvfb_fbdir, request):
+    global current_compose_project
+    assert current_compose_project is None
+
     os.environ['DDTERM_TEST_XVFB_FBDIR'] = str(xvfb_fbdir)
     project = container_util.ComposeProject()
+    current_compose_project = project
 
     yield project
 
-    project.down()
+    teardown_compose_project()
 
 
 @pytest.fixture(scope='session')
